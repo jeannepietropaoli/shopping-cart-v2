8674d379ee8ca5e3974552c568a74dbe52b2c1ee
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-16 16:43:29 +0200

    Create NoItemsToShow to fallback if products fetching fails

---
+++ src/components/shared/NoItemToShow.jsx
@@ -0,0 +1,9 @@
+export default function NoItemsToShow() {
+    return (
+        <div className="no-items-error">
+            <p>Oops, no items to show for now. </p>
+            <p>It looks like the customers have cleaned us out!</p>
+            <p>We are working on it, come back later ! 🍇</p>
+        </div>
+    )
+}
