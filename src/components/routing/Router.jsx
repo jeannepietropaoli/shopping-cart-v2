8c681cdd2eb5867a5a22804d73e9afafd4e2cc7a
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-17 00:21:55 +0200

    Pass props to routes

--- src/components/routing/Router.jsx
+++ src/components/routing/Router.jsx
@@ -1,23 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import App from "../App"
+import Layout from "../layout/Layout"
 import Home from "../layout/Home/Home"
 import Shop from "../layout/Shop/Shop"
 import GeneralErrorPage from "./errorPages/GeneralErrorPage"
 
-export default function Router() {
+export default function Router({products, error, loading}) {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <App/>,
+            element: <Layout/>,
             errorElement: <GeneralErrorPage/>,
             children: [
                 {
                     path: 'home',
-                    element: <Home/>,
+                    element: <Home products={products} error={error} loading={loading}/>,
                 },
                 {
                     path: 'shop',
-                    element: <Shop/>,
+                    element: <Shop products={products} error={error} loading={loading}/>,
                 },
                 {
                     path: '*',
