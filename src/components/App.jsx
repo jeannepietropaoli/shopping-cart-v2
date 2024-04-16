7c6d1640d841ad2ad7d09e92d995b09ac757f0c2
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-16 16:33:01 +0200

    Create App component and data management logic

---
+++ src/components/App.jsx
@@ -0,0 +1,40 @@
+import Layout from "./layout/Layout";
+import useFetch from "../customHooks/useFetch"
+import { FetchDataContext } from "../contexts/FetchDataContext";
+import '../styles/App.css'
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export default function App() {
+    const {
+        fetchedData: products,
+        error,
+        loading,
+      } = useFetch("https://api.api-ninjas.com/v1/emoji?subgroup=food_fruit", {
+        "X-Api-Key": apiKey,
+      });
+
+    const getFakePrice = () => {
+        const minPrice = 1
+        const maxPrice = 8
+        return Math.floor(Math.random() * maxPrice) + minPrice
+    }
+      const addFakeDetailsToProducts = (products) => {
+        return products 
+            ? products.map(product => {
+                return ({...product, id: product.code, price: getFakePrice()})
+            })
+            : []
+      }
+
+      const detailedProducts = addFakeDetailsToProducts(products)
+    
+    return (
+        <div className="app">
+            <FetchDataContext.Provider value={{ products: detailedProducts, error, loading }}>
+                <Layout />
+            </FetchDataContext.Provider>
+        </div>
+    )
+}
+
