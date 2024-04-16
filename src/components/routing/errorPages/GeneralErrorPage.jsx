3f9f100ad79e8bcbf639b79c6a86af473fffb1ac
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-16 16:41:57 +0200

    Create GeneralErrorPage to fallback in case of invalid route

---
+++ src/components/routing/errorPages/GeneralErrorPage.jsx
@@ -0,0 +1,11 @@
+import '../../../styles/GeneralErrorPage.css'
+
+export default function GeneralErrorPage() {
+    return (
+        <section id='general-error-page'>
+            <div className="container">
+                <p>Oh oh, seems like something went wrong.</p>
+            </div>
+        </section>
+    )
+}
