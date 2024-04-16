bb84f86a45dd0f645d075a983526b417c1828416
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-16 16:47:34 +0200

    Create custom useFetch hook to fetch data

---
+++ src/customHooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url, headers=null) {
+  const [fetchedData, setFetchedData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url, {
+        headers: headers
+      })
+      .then(response => {
+          if(!response.ok) {
+              throw new Error(`An error has occured, server returned ${response.status}`)
+          }
+          return response.json()
+      })
+      .then(data => {
+        if(data.length === 0) {
+          throw new Error('Error: empty array returned, no data')
+        }
+        setFetchedData(data)
+      })
+      .catch(error => {
+        console.error(error)
+        setError(error)
+      })
+      .finally(() => setLoading(false))
+    }, 4000)
+        
+  }, []);
+
+  return {fetchedData, loading, error};
+}
