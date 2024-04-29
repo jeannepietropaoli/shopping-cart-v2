ea582ca81d7280ab7ab17d1182ef7d91208f89f0
Jeanne <j.pietropaoli@yahoo.fr>
2024-04-29 21:41:56 +0200

    Modify Home's path

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -2,14 +2,14 @@ import Navbar from "./Navbar"
 import '../../../styles/Header.css'
 import { Link } from "react-router-dom"
 
-export default function Header() {
+export default function Header({numberOfProductsInCart}) {
     return(
         <header>
             <div className='container'>
-                <Link to='/home'>
+                <Link to='/'>
                     <h1>fruictify.</h1>
                 </Link>
-                <Navbar />
+                <Navbar numberOfProductsInCart={numberOfProductsInCart} />
             </div>
         </header>
     )
