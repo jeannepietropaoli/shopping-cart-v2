e5dd08549d3a6941f84781fa1dcee98eef7a3a1b
Jeanne <j.pietropaoli@yahoo.fr>
2024-05-07 21:54:25 +0200

    Header: Add prop validation

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,16 +1,21 @@
 import Navbar from "./Navbar"
 import '../../../styles/Header.css'
 import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
 
 export default function Header({numberOfProductsInCart}) {
     return(
-        <header>
-            <div className='container'>
-                <Link to='/'>
-                    <h1>fruictify.</h1>
+        <header className="header">
+            <div className='container header__container'>
+                <Link to='/' className="header__logo">
+                    fruictify.
                 </Link>
                 <Navbar numberOfProductsInCart={numberOfProductsInCart} />
             </div>
         </header>
     )
+}
+
+Header.propTypes = {
+    numberOfProductsInCart: PropTypes.number
 }
