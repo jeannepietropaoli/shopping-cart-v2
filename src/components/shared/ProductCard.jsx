460c221e7128ce17d8a675b0f021ef5f61236a7c
Jeanne <j.pietropaoli@yahoo.fr>
2024-05-07 21:55:42 +0200

    ProductCard: Simplify component and switch to our own Context

--- src/components/shared/ProductCard.jsx
+++ src/components/shared/ProductCard.jsx
@@ -1,19 +1,22 @@
 import Loader from "./Loader";
 import addToCartIcon from '../../assets/shopping-bag.png'
 import '../../styles/ProductCard.css'
+import { useContext } from "react";
+import { CartActionsContext } from "../contexts/CartActionsContext";
 
-export default function ProductCard({item, showPrice=false, showAddToCartBtn=false, addProdcutToCart }) {
+export default function ProductCard({item}) {
+    const { addProductToCart } = useContext(CartActionsContext)
     return (
-        <div className="product-card">
+        <div className="product-card" data-testid="product-card">
             {!item && <Loader />}
             {item &&
                 <>
                     <img className="product-card__img" src={item.image} alt={`${item.name}'s emoji`} />
                     <div className="product-card__infos">
                         <p className="product-card__info">{item.name}</p>
-                        {showPrice && <p className="product-card__info">{item.price}$</p>}
+                        <p className="product-card__info">{item.price}$</p>
                     </div>
-                    {showAddToCartBtn && <button onClick={() => addProdcutToCart(item)} className="product-card__add-to-cart-btn"><img className="product-card__add-to-cart-img" src={addToCartIcon} alt="add-to-cart icon" /></button>}
+                    <button onClick={() => addProductToCart(item)} className="product-card__add-to-cart-btn"><img className="product-card__add-to-cart-img" src={addToCartIcon} alt="add-to-cart icon" /></button>
                 </>
             }
         </div>
