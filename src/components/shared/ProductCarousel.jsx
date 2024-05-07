b149a2641abecf5a7e0ae25d9be64dc3a87f68ec
Jeanne <j.pietropaoli@yahoo.fr>
2024-05-07 21:56:45 +0200

    Simplify Carousel and and our own ProductsContext

--- src/components/shared/ProductCarousel.jsx
+++ src/components/shared/ProductCarousel.jsx
@@ -1,21 +1,54 @@
-import ProductCard from "./ProductCard";
+import "../../styles/ProductCarousel.css";
+import { useContext } from "react";
+import { ProductsContext } from "../contexts/ProductsContext";
+import Loader from "./Loader";
 import NoItemsToShow from "./NoItemToShow";
-import Carousel from "./Carousel";
 
-export default function ProductCarousel({products, error, loading}) {
+function ProductCarouselItem({item}) {
+  return (
+    <>
+        {!item && <Loader />}
+        {item &&
+            <>
+                <img className="carousel__img" src={item.image} alt={`${item.name}'s emoji`} />
+                <p className="carousel__infos">{item.name}</p>
+            </>
+        }
+    </>
+)
+}
+
+function ProductCarouselSlider({itemsToDisplay}) {
+  return (
+    <div className="carousel__slider">
+        {itemsToDisplay.map((item, index) => {
+          return (
+            <div key={index} className="carousel__item" data-testid='carousel-item'>
+              <ProductCarouselItem item={item} />
+            </div>
+          )
+        })}
+    </div>
+  )
+}
+
+export default function ProductCarousel() {
+  const { products, error, loading } = useContext(ProductsContext)
   const numberOfLoadingProducts = 5;
   const arrayOfLoadingProducts = Array(numberOfLoadingProducts).fill(null);
-  const excractNFirstProducts = (products, count) => {
-    return products.slice(0, count);
+  const excractNFirstProducts = (products, n) => {
+    return products.slice(0, n);
   };
-  const productsToDisplay = loading ? arrayOfLoadingProducts : excractNFirstProducts(products, numberOfLoadingProducts);
-  const renderProdcut = (item) => <ProductCard item={item} fullyDetailed={false}/>
+  const itemsToDisplay = loading ? arrayOfLoadingProducts : excractNFirstProducts(products, numberOfLoadingProducts);
 
   if (error) {
     return <NoItemsToShow />;
   }
-
-  return(
-    <Carousel itemsToDisplay={productsToDisplay} renderItem={renderProdcut} />
-  )
+  return (
+    <div className="carousel" data-testid="carousel">
+      {/* duplication of slider allows to create an infinite carousel of items */}
+      <ProductCarouselSlider itemsToDisplay={itemsToDisplay} />
+      <ProductCarouselSlider itemsToDisplay={itemsToDisplay} />
+    </div>
+  );
 }
