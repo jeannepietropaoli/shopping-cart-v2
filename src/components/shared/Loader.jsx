2e5f2f20229bc7245759d8ed35a4e588a0136c3c
Jeanne <j.pietropaoli@yahoo.fr>
2024-05-07 21:55:05 +0200

    Loader: Add a data-testid

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -2,6 +2,6 @@ import '../../styles/Loader.css'
 
 export default function Loader() {
     return (
-        <div className="loader"></div>
+        <div className="loader" data-testid="loader"></div>
     )
 }
