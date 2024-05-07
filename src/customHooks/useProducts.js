62bdc1675b1c184454661cc8fe43199b7638e2f8
Jeanne <j.pietropaoli@yahoo.fr>
2024-05-07 21:58:49 +0200

    Isolate a custom useProducts' hook

---
+++ src/customHooks/useProducts.js
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+import useFetch from "./useFetch";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+
+export default function useProducts() {
+    const { fetchedData, error, loading } = useFetch("https://api.api-ninjas.com/v1/emoji?subgroup=food_fruit", {"X-Api-Key": apiKey});
+
+    const getFakePrice = () => {
+        const minPrice = 1;
+        const maxPrice = 8;
+        return Math.floor(Math.random() * maxPrice) + minPrice;
+      };
+    
+      const addFakeDetailsToProducts = (products) => {
+        return products
+          ? products.map((product) => {
+              return { ...product, id: product.code, price: getFakePrice() };
+            })
+          : [];
+      };
+
+      const products = useMemo(() => addFakeDetailsToProducts(fetchedData), [fetchedData])
+
+      return { products, error, loading }
+}
